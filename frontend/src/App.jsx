87a7d0fe3ae415b6ab752e3b7d5f19280592fac9
AndresilloDev <149657811+AndresilloDev@users.noreply.github.com>
2025-10-29 12:18:03 -0600

    Initial commit - frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline text-blue-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
